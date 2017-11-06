e6764d1513965c6b573dc5e7374e44df93b559c6
Dario Litz <d.litz@lifecodexx.com>
2017-11-06 22:14:56 +0100

    Projektdateien hinzufügen.

---
+++ Taschenrechner/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.divideButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.subtractButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.commaButton = new System.Windows.Forms.Button();
+            this.zeroButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.threeButton = new System.Windows.Forms.Button();
+            this.twoButton = new System.Windows.Forms.Button();
+            this.oneButton = new System.Windows.Forms.Button();
+            this.sixButton = new System.Windows.Forms.Button();
+            this.fiveButton = new System.Windows.Forms.Button();
+            this.fourButton = new System.Windows.Forms.Button();
+            this.nineButton = new System.Windows.Forms.Button();
+            this.eightButton = new System.Windows.Forms.Button();
+            this.sevenButton = new System.Windows.Forms.Button();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.resultBox, 0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // tableLayoutPanel2
+            // 
+            resources.ApplyResources(this.tableLayoutPanel2, "tableLayoutPanel2");
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            // 
+            // tableLayoutPanel4
+            // 
+            resources.ApplyResources(this.tableLayoutPanel4, "tableLayoutPanel4");
+            this.tableLayoutPanel4.Controls.Add(this.calcButton, 4, 0);
+            this.tableLayoutPanel4.Controls.Add(this.divideButton, 3, 0);
+            this.tableLayoutPanel4.Controls.Add(this.multiplyButton, 2, 0);
+            this.tableLayoutPanel4.Controls.Add(this.subtractButton, 1, 0);
+            this.tableLayoutPanel4.Controls.Add(this.addButton, 0, 0);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            // 
+            // calcButton
+            // 
+            resources.ApplyResources(this.calcButton, "calcButton");
+            this.calcButton.Name = "calcButton";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // divideButton
+            // 
+            resources.ApplyResources(this.divideButton, "divideButton");
+            this.divideButton.Name = "divideButton";
+            this.divideButton.UseVisualStyleBackColor = true;
+            this.divideButton.Click += new System.EventHandler(this.divideButton_Click);
+            // 
+            // multiplyButton
+            // 
+            resources.ApplyResources(this.multiplyButton, "multiplyButton");
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.multiplyButton_Click);
+            // 
+            // subtractButton
+            // 
+            resources.ApplyResources(this.subtractButton, "subtractButton");
+            this.subtractButton.Name = "subtractButton";
+            this.subtractButton.UseVisualStyleBackColor = true;
+            this.subtractButton.Click += new System.EventHandler(this.subtractButton_Click);
+            // 
+            // addButton
+            // 
+            resources.ApplyResources(this.addButton, "addButton");
+            this.addButton.Name = "addButton";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            resources.ApplyResources(this.tableLayoutPanel3, "tableLayoutPanel3");
+            this.tableLayoutPanel3.Controls.Add(this.commaButton, 2, 3);
+            this.tableLayoutPanel3.Controls.Add(this.zeroButton, 1, 3);
+            this.tableLayoutPanel3.Controls.Add(this.clearButton, 0, 3);
+            this.tableLayoutPanel3.Controls.Add(this.threeButton, 2, 2);
+            this.tableLayoutPanel3.Controls.Add(this.twoButton, 1, 2);
+            this.tableLayoutPanel3.Controls.Add(this.oneButton, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.sixButton, 2, 1);
+            this.tableLayoutPanel3.Controls.Add(this.fiveButton, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.fourButton, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.nineButton, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.eightButton, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.sevenButton, 0, 0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            // 
+            // commaButton
+            // 
+            resources.ApplyResources(this.commaButton, "commaButton");
+            this.commaButton.Name = "commaButton";
+            this.commaButton.UseVisualStyleBackColor = true;
+            this.commaButton.Click += new System.EventHandler(this.commaButton_Click);
+            // 
+            // zeroButton
+            // 
+            resources.ApplyResources(this.zeroButton, "zeroButton");
+            this.zeroButton.Name = "zeroButton";
+            this.zeroButton.UseVisualStyleBackColor = true;
+            this.zeroButton.Click += new System.EventHandler(this.zeroButton_Click);
+            // 
+            // clearButton
+            // 
+            resources.ApplyResources(this.clearButton, "clearButton");
+            this.clearButton.Name = "clearButton";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // threeButton
+            // 
+            resources.ApplyResources(this.threeButton, "threeButton");
+            this.threeButton.Name = "threeButton";
+            this.threeButton.UseVisualStyleBackColor = true;
+            this.threeButton.Click += new System.EventHandler(this.threeButton_Click);
+            // 
+            // twoButton
+            // 
+            resources.ApplyResources(this.twoButton, "twoButton");
+            this.twoButton.Name = "twoButton";
+            this.twoButton.UseVisualStyleBackColor = true;
+            this.twoButton.Click += new System.EventHandler(this.twoButton_Click);
+            // 
+            // oneButton
+            // 
+            resources.ApplyResources(this.oneButton, "oneButton");
+            this.oneButton.Name = "oneButton";
+            this.oneButton.UseVisualStyleBackColor = true;
+            this.oneButton.Click += new System.EventHandler(this.oneButton_Click);
+            // 
+            // sixButton
+            // 
+            resources.ApplyResources(this.sixButton, "sixButton");
+            this.sixButton.Name = "sixButton";
+            this.sixButton.UseVisualStyleBackColor = true;
+            this.sixButton.Click += new System.EventHandler(this.sixButton_Click);
+            // 
+            // fiveButton
+            // 
+            resources.ApplyResources(this.fiveButton, "fiveButton");
+            this.fiveButton.Name = "fiveButton";
+            this.fiveButton.UseVisualStyleBackColor = true;
+            this.fiveButton.Click += new System.EventHandler(this.fiveButton_Click);
+            // 
+            // fourButton
+            // 
+            resources.ApplyResources(this.fourButton, "fourButton");
+            this.fourButton.Name = "fourButton";
+            this.fourButton.UseVisualStyleBackColor = true;
+            this.fourButton.Click += new System.EventHandler(this.fourButton_Click);
+            // 
+            // nineButton
+            // 
+            resources.ApplyResources(this.nineButton, "nineButton");
+            this.nineButton.Name = "nineButton";
+            this.nineButton.UseVisualStyleBackColor = true;
+            this.nineButton.Click += new System.EventHandler(this.nineButton_Click);
+            // 
+            // eightButton
+            // 
+            this.eightButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.eightButton, "eightButton");
+            this.eightButton.Name = "eightButton";
+            this.eightButton.UseVisualStyleBackColor = true;
+            this.eightButton.Click += new System.EventHandler(this.eightButton_Click);
+            // 
+            // sevenButton
+            // 
+            resources.ApplyResources(this.sevenButton, "sevenButton");
+            this.sevenButton.Name = "sevenButton";
+            this.sevenButton.UseVisualStyleBackColor = true;
+            this.sevenButton.Click += new System.EventHandler(this.sevenButton_Click);
+            // 
+            // resultBox
+            // 
+            resources.ApplyResources(this.resultBox, "resultBox");
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.UseWaitCursor = true;
+            this.resultBox.TextChanged += new System.EventHandler(this.resultBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calcButton;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button divideButton;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button subtractButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button commaButton;
+        private System.Windows.Forms.Button zeroButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button threeButton;
+        private System.Windows.Forms.Button twoButton;
+        private System.Windows.Forms.Button oneButton;
+        private System.Windows.Forms.Button sixButton;
+        private System.Windows.Forms.Button fiveButton;
+        private System.Windows.Forms.Button fourButton;
+        private System.Windows.Forms.Button nineButton;
+        private System.Windows.Forms.Button eightButton;
+        private System.Windows.Forms.Button sevenButton;
+        public System.Windows.Forms.TextBox resultBox;
+    }
+}
+
---
+++ Taschenrechner/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Taschenrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resultBox_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += " + ";
+        }
+
+        private void subtractButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += " - ";
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += " * ";
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += " / ";
+        }
+
+        private string calcStrings(string num1, string op, string num2)
+        {
+            float number1 = float.Parse(num1);
+            float number2 = float.Parse(num2);
+
+            switch (op)
+            {
+                case "+":
+                    return (number1 + number2).ToString();
+
+                case "-":
+                    return (number1 - number2).ToString();
+
+                case "*":
+                    return (number1 * number2).ToString();
+
+                case "/":
+                    return (number1 / number2).ToString();
+
+                default:
+                    return "error";
+            }
+        }
+
+        private readonly string[] PlusMinus = new string[] { "+", "-" };
+        private readonly string[] MultDiv = new string[] { "*", "/" };
+
+        private List<string> DeleteElements(List<string> ary, int[] indexes)
+        {
+            List<string> newAry = new List<string>();
+            for (int i = 0; i < ary.Count; i++)
+            {
+                if (!indexes.Contains(i))
+                {
+                    newAry.Add(ary[i]);
+                }
+            }
+            return newAry;
+        }
+
+        private List<string> Process(List<string> splittedText, int counter, string[] ops)
+        {
+            if (counter + 1 >= splittedText.Count) return splittedText;
+            if (ops.Contains(splittedText[counter + 1]))
+            {
+                splittedText[counter] = calcStrings(splittedText[counter], splittedText[counter + 1], splittedText[counter + 2]);
+                splittedText = DeleteElements(splittedText, new int[] { counter + 1, counter + 2 });
+                return Process(splittedText, counter, ops);
+            }
+            else return splittedText;
+        }
+
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            List<string> splittedText = resultBox.Text.Split(' ').ToList();
+            int counter = 0;
+            while (counter <= splittedText.Count)
+            {
+                if (splittedText.Count == 1)
+                {
+                    resultBox.Text = splittedText[0];
+                    break;
+                }
+                if (MultDiv.Contains(splittedText[counter + 1]))
+                {
+                    splittedText = Process(splittedText, counter, MultDiv);
+                    counter = 0; ;
+                }
+                else
+                {
+                    if (counter + 3 >= splittedText.Count || PlusMinus.Contains(splittedText[counter + 3]))
+                    {
+                        splittedText = Process(splittedText, counter, PlusMinus);
+                    }
+                    else counter++;
+                }
+            }
+        }
+
+        private void sevenButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "7";
+        }
+
+        private void eightButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "8";
+        }
+
+        private void nineButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "9";
+        }
+
+        private void fourButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "4";
+        }
+
+        private void fiveButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "5";
+        }
+
+        private void sixButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "6";
+        }
+
+        private void oneButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "1";
+        }
+
+        private void twoButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "2";
+        }
+
+        private void threeButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "3";
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = "";
+        }
+
+        private void zeroButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += "0";
+        }
+
+        private void commaButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text += ",";
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case (char)48:
+                    zeroButton_Click(sender, e);
+                    break;
+
+                case (char)49:
+                    oneButton_Click(sender, e);
+                    break;
+
+                case (char)50:
+                    twoButton_Click(sender, e);
+                    break;
+
+                case (char)51:
+                    threeButton_Click(sender, e);
+                    break;
+
+                case (char)52:
+                    fourButton_Click(sender, e);
+                    break;
+
+                case (char)53:
+                    fiveButton_Click(sender, e);
+                    break;
+
+                case (char)54:
+                    sixButton_Click(sender, e);
+                    break;
+
+                case (char)55:
+                    sevenButton_Click(sender, e);
+                    break;
+
+                case (char)56:
+                    eightButton_Click(sender, e);
+                    break;
+
+                case (char)57:
+                    nineButton_Click(sender, e);
+                    break;
+
+                case (char)8:
+                    clearButton_Click(sender, e);
+                    break;
+
+                case (char)44:
+                    commaButton_Click(sender, e);
+                    break;
+
+                case (char)42:
+                    multiplyButton_Click(sender, e);
+                    break;
+
+                case (char)43:
+                    addButton_Click(sender, e);
+                    break;
+
+                case (char)45:
+                    subtractButton_Click(sender, e);
+                    break;
+
+                case (char)47:
+                    divideButton_Click(sender, e);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
